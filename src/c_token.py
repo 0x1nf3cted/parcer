35b96b85e4ce44495faebc395ff339d40a01642b
duckduckcodes <0x07d2@proton.me>
2023-09-24 19:57:33 +0200

    added AST Class structure

--- src/c_token.py
+++ src/c_token.py
@@ -7,3 +7,112 @@ class Token:
 
     def __str__(self):
         return f"Token(type='{self.type}', value='{self.value}', line_number={self.line_number}, position={self.position})"
+
+
+class ASTNode:
+    def __init__(self, node_type, value=None, position=None):
+        self.node_type = node_type
+        self.value = value
+        self.position = position
+        self.children = []
+        self.parent = None
+
+    def add_child(self, child_node):
+        child_node.parent = self
+        self.children.append(child_node)
+
+    def to_string(self, level=0):
+        indent = "  " * level
+        node_str = f"{indent}{self.node_type}"
+        if self.value is not None:
+            node_str += f": {self.value}"
+        if self.position is not None:
+            node_str += f" ({self.position})"
+        result = [node_str]
+        for child in self.children:
+            result.extend(child.to_string(level + 1).split('\n'))
+        return '\n'.join(result)
+
+# class for Number nodes
+class BinaryOperatorNode(ASTNode):
+    def __init__(self, operator, left, right):
+        self.operator = operator
+        self.left = left
+        self.right = right
+
+# class for Array nodes 
+class ArrayNode(ASTNode):
+    def __init__(self, name: str, size: int, elements):
+        self.name = name
+        self.size = size
+        self.elements = elements
+
+# class for Struct nodes 
+class StructNode(ASTNode):
+    def __init__(self, name,  elements):
+        self.name = name
+        self.elements = elements
+
+
+# class for Macro nodes
+class MacroNode(ASTNode):
+    def __init__(self, name, macro_body):
+        self.name = name
+        self.body = macro_body
+
+
+# class for nodes (character, int, float, string, ect...)
+class VariableNode(ASTNode):
+    def __init__(self, value):
+        self.value = value
+
+
+# class for Identifiers
+class IdentifierNode(ASTNode):
+    def __init__(self, name):
+        self.name = name
+
+
+# class for Asignements
+class AssignmentNode(ASTNode):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+# class for Conditionals
+class IfStatementNode(ASTNode):
+    def __init__(self, condition, body, else_body=None):
+        self.condition = condition
+        self.body = body
+        self.else_body = else_body
+
+
+# class for While loop
+class WhileLoopNode(ASTNode):
+    def __init__(self, condition, body):
+        self.condition = condition
+        self.body = body
+
+
+# class for Function declaration
+class FunctionDeclarationNode(ASTNode):
+    def __init__(self, return_type, name, parameters, body):
+        self.return_type = return_type
+        self.name = name
+        self.parameters = parameters
+        self.body = body
+
+
+# class for Function calls
+class FunctionCallNode(ASTNode):
+    def __init__(self, name, arguments):
+        self.name = name
+        self.arguments = arguments
+
+# class for For loop
+class ForLoopNode(ASTNode):
+    def __init__(self, initialization, condition, update, body):
+        self.initialization = initialization
+        self.condition = condition
+        self.update = update
+        self.body = body
